0a0d1bb0659202d1c78c97a273bf6289f837fe1c
aliyaman <aliymn.db@gmail.com>
2017-01-11 00:58:16 +0300

    pep8 update

--- alexa.py
+++ alexa.py
@@ -28,29 +28,58 @@ class AlexaInfo():
         soup = BeautifulSoup(urllib.request.urlopen("http://www.alexa.com/siteinfo/{}".format(self.url)),'html.parser')
         self.soup = soup
 
-    def alexaInfo(self):
-        """Dünya, ülke, çıkma oranı, backlink gibi genel alexa bilgileri vermektedir."""
+    def ranks(self) -> dict:
+        """
+        Alexa.com üzerinden, genel rankların bilgisini verdiği fonksiyondur.
+
+        # Properties #
+        global_rank         | Sitenin, global ülke puanı.
+        country_rank        | Sitenin, ülke içerisindeki puanı.
+        bounce_rate         | Alexa üzerinde puan yükseltme oranı.
+        views               | Sitenin, görüntülenme yüzdesi.
+        views_hours         | Sitenin, saatlik görütünlenme yüzdesi
+        search_engine       | Arama motorunda aranma hacmi
+        backlink            | Alexa backlink değeri.
+
+        """
+
+        #Eğer site yeniyle, default '0' değeri atamaktadır.
         global_rank, country_rank, bounce_rate, views, views_hours, search_engine, backlink = "0","0","0","0","0","0","0"
+
         # Rankları filitre ettik.
-        ranks = self.soup.find_all('strong', attrs={'class': 'metrics-data align-vmiddle'})
+        rank = self.soup.find_all('strong', attrs={'class': 'metrics-data align-vmiddle'})
+
+        #Elde edilen bilgileri,değişkenlere aktar
         country_name = self.soup.find_all('span', attrs={'class': 'countryRank'})[0].find("h4").find("a").text
-        global_rank = ranks[0].text.strip() # Dünya Rankı
-        country_rank = ranks[1].text.strip()# Ülke rankı
-        bounce_rate = ranks[2].text.strip() # Çıkma Oranı
-        views = ranks[3].text.strip() # Günlük ziyaretçi başına düşen, görüntülenme sayısı
-        views_hours = ranks[4].text.strip() # Sitedeki günlük saat görüntülenme sayısı
-        if(len(ranks) == 5):
-            ranks.insert(4,"")
-        search_engine = ranks[5].text.strip() # Aramaların % kaçı arama motorundan gerçekleşiyor?
+        global_rank = rank[0].text.strip() # Dünya Rankı
+        country_rank = rank[1].text.strip()# Ülke rankı
+        bounce_rate = rank[2].text.strip() # Çıkma Oranı
+        views = rank[3].text.strip() # Günlük ziyaretçi başına düşen, görüntülenme sayısı
+        views_hours = rank[4].text.strip() # Sitedeki günlük saat görüntülenme sayısı
+
+        #Serach_engine kontrol
+        if(len(rank) == 5):
+            rank.insert(4,"")
+
+        # Aranma Hacmi
+        search_engine = rank[5].text.strip()
+        # Alexa backlink
         backlink = self.soup.find_all('span',attrs={"class":'font-4 box1-r'})[0].text
-        info_list = ["Global Rank", "Name of country", "Rank in Country ", "Bounce Rate", "Daily Pageviews per Visitor",
-                     "Daily Time on Site", "Search Traffic", "Backlink"]
+
+        #Dict Key
+        info_list = ["Global Rank", "Name of country",
+                     "Rank in Country ", "Bounce Rate",
+                     "Daily Pageviews per Visitor","Daily Time on Site",
+                     "Search Traffic", "Backlink"]
+        #Dict Value
         result = [global_rank,country_name,country_rank,bounce_rate,views,views_hours,search_engine,backlink]
+
+        #Dict
         list_to_dict = dict(zip(result,info_list))
 
         return list_to_dict
 
-    def alaxaKeywords(self):
+    def keywords(self) -> dict:
         """5 popüler kelime ve oranı bilgileri verir."""
         keywords_name = []
         keywords_value = []
@@ -67,45 +96,59 @@ class AlexaInfo():
 
         return list_to_dict
 
-    def upstreamSites(self):
-        """5 adet popüler ziyaretçi alınan siteden ismi ve oranını verir."""
+    def visit(self)->dict:
+        """En çok ziyaret gelen siteleri listeler."""
+
         site_name = []
         site_value = []
+
         ups_site = self.soup.find_all('table', attrs={'id': 'keywords_upstream_site_table'})[0].find_all("a")
         ups_value = self.soup.find_all('table', attrs={'id': 'keywords_upstream_site_table'})[0].find_all('span',attrs={"class":""})
 
         for i in ups_value:
+            #Key
             site_value.append(i.text)
 
         for k in ups_site:
+            #Value
             site_name.append(k.text)
 
+        #Dict
         list_to_dict = dict(zip(site_value,site_name))
 
         return list_to_dict
-    def alexaRival(self):
+
+    def rival(self) -> tuple:
         """Ortak ziyaretçiye sahip 5 adet site bilgisi verir. Rakip sitelerde diyebiliriz."""
+
         site_name = []
+        #Bilgiler alınıyor.
         rival_site = self.soup.find_all('table', attrs={'id': 'audience_overlap_table'})[0].find_all("a")
+
         for i in rival_site:
+            #Value
             site_name.append(i.text)
 
         return tuple(site_name)
 
-    def subDomain(self):
+    def sub_domain(self) -> dict:
         """Sub Domainler üzerinden aldığınız trafik yüzdesini gösterir."""
+
         site_name = []
         site_value = []
 
+        #Bilgiler alınıyor.
         sub_name = self.soup.find_all('table', attrs={'id': 'subdomain_table'})[0].find_all("span",attrs={"class":"word-wrap"})
         sub_value = self.soup.find_all('table', attrs={'id': 'subdomain_table'})[0].find_all('span',attrs={"class":""})
 
         for i in sub_name:
+            #Key
             site_name.append(i.text)
 
         for k in sub_value:
+            #Value
              site_value.append(k.text)
-
+        #Dict
         list_to_dict = dict(zip(site_value, site_name))
 
         return list_to_dict
@@ -114,17 +157,17 @@ if __name__ == '__main__':
     alexa = AlexaInfo("python.tc")
 
     print("Site Genel Bilgiler;")
-    print(alexa.alexaInfo())
+    print(alexa.ranks())
 
     print("\nSalexaRivalite 5 popüler keywords")
-    print(alexa.alaxaKeywords())
+    print(alexa.keywords())
 
     print("\nZiyaretçilerin nereden geldiği;")
-    print(alexa.upstreamSites())
+    print(alexa.visit())
 
     print("\nRakip Analizi;")
-    print(alexa.alexaRival())
+    print(alexa.rival())
 
     print("\nSubDomain Ziyaretçi Analizi")
-    print(alexa.subDomain())
+    print(alexa.sub_domain())
 
